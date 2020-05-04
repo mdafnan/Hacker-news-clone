7d96d897aed50bb1adeebe060f4b2fec1a05050f
MA068408 <mdafnan88@gmail.com>
2020-05-04 13:34:03 +0530

    Updates

---
+++ precache-manifest.ff8be91bf87138f327335ce319ca64e1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82d72f686b8d50eb965fdc11c8046f99",
+    "url": "/Hacker-news-clone/index.html"
+  },
+  {
+    "revision": "213d46bb9ffff6032510",
+    "url": "/Hacker-news-clone/static/css/2.df6466d6.chunk.css"
+  },
+  {
+    "revision": "b5a9b4cd7600598e1158",
+    "url": "/Hacker-news-clone/static/css/main.90af3711.chunk.css"
+  },
+  {
+    "revision": "213d46bb9ffff6032510",
+    "url": "/Hacker-news-clone/static/js/2.88539408.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Hacker-news-clone/static/js/2.88539408.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5a9b4cd7600598e1158",
+    "url": "/Hacker-news-clone/static/js/main.d7c6b88c.chunk.js"
+  },
+  {
+    "revision": "194a07c34f0b653797dd",
+    "url": "/Hacker-news-clone/static/js/runtime-main.0b77b9b5.js"
+  }
+]);
